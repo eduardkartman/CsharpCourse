83e77ca2ce3261d8e34b5fe522689311dccf2600
Eduard Kartman <eduard.kartman@yahoo.com>
2023-10-03 11:37:39 +0300

    Finishing section

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,4 +1,6 @@
-﻿/*var numbers = new SimpleList<int>();
+﻿using System.Numerics;
+using System.Xml;
+/*var numbers = new SimpleList<int>();
 numbers.Add(10);
 numbers.Add(20);
 numbers.Add(30);
@@ -14,29 +16,110 @@ words.Add("ccc");
 var dates = new SimpleList<DateTime>();
 dates.Add(new DateTime(2025, 1, 6));
 dates.Add(new DateTime(2025, 6, 6));*/
-
 /*var numbers = new List<int> { 5, 3, 2, 8, 16, 7 };
 SimpleTuple<int,int> mindAndMax = GetMinAndMax(numbers);
 Console.WriteLine("Minimum is : " + mindAndMax.Item1);
 Console.WriteLine("Maximum is : " + mindAndMax.Item2);*/
-
-using System.Numerics;
-
-Console.WriteLine("Square of 2 is : " + Calculator.Square(2));
+/*Console.WriteLine("Square of 2 is : " + Calculator.Square(2));
 Console.WriteLine("Square of 4m is : " + Calculator.Square(4m));
 Console.WriteLine("Square of 6d is : " + Calculator.Square(6d));
+*/
+/*
+ProcessString processString1 = TrimTo5Letters;
+ProcessString processString2 = ToUpper;
+Console.WriteLine(processString1("Helloooooooooo"));
+Console.WriteLine(processString2("Helloooooooooo"));*/
+/*var countryToCurrencyMapping = new Dictionary<string, string>
+{
+    ["USA"] = "USD",
+    ["India"] = "INR",
+    ["Spain"] = "EUR",
+};
+if (countryToCurrencyMapping.ContainsKey("Italy"))
+{
+    Console.WriteLine("Currency in Italy is " +
+        countryToCurrencyMapping["Italy"]);
+}
+countryToCurrencyMapping["Poland"] = "PLN";
+foreach (var country in countryToCurrencyMapping)
+{
+    Console.WriteLine($"Country: {country.Key}, " +
+        $"currency: {country.Value}");
+}
+*/
+/* Employees exercise with Dictionary
+    var employees = new List<Employee>
+{
+    new Employee("Jake Smith" , "Space Navigation", 25000),
+    new Employee("Anna Blake" , "Space Navigation", 29000),
+    new Employee("Barbara Oak" , "Xenobiology", 21500),
+    new Employee("Damien Parker" , "Xenobiology", 22000),
+    new Employee("Nisha Patel" , "Mechanics", 21000),
+    new Employee("Gustavo Sanchez" , "Mechanics", 20000),
+
+};
+Dictionary<string, decimal> CalculateAverageSalaryPerDepartment(
+    IEnumerable<Employee> employees)
+{
+    var employeesPerDepartments = new Dictionary<string, List<Employee>>();
+
+    foreach (var employee in employees)
+    {
+        if (!employeesPerDepartments.ContainsKey(employee.Department))
+        {
+            employeesPerDepartments[employee.Department] = new List<Employee>();
+        }
+
+        employeesPerDepartments[employee.Department].Add(employee);
+    }
+    var result = new Dictionary<string, decimal>();
+
+    foreach(var employeesPerDepartment in employeesPerDepartments)
+    {
+        decimal sum = 0;
+        foreach(var employee in employeesPerDepartment.Value)
+        {
+            sum += employee.MonthlySalary;
+        }
+        var avg = sum / employeesPerDepartment.Value.Count;
+
+        result[employeesPerDepartment.Key] = avg;
+    }
+
+    return result;
+}
+public class Employee
+{
+    public Employee(string name, string department, decimal monthlySalary)
+    {
+        Name = name;
+        Department = department;
+        MonthlySalary = monthlySalary;
+    }
+    public string Name { get; init; }
+    public string Department { get; init; }
+    public decimal MonthlySalary { get; init; }
 
 
-Console.ReadKey();
+}
 
-public static class Calculator
+*/
+/* string TrimTo5Letters(string input)
+{
+    return input.Substring(0, 5);
+}
+
+string ToUpper(string input)
+{
+    return input.ToUpper();
+}
+delegate string ProcessString(string input);*/
+/*public static class Calculator
 {
     public static T Square<T>(T input) where T : INumber<T> 
         => input * input;
 }
-
-
-
+*/
 /*
 SimpleTuple<int, int> GetMinAndMax(IEnumerable<int> input)
 {
@@ -126,3 +209,66 @@ class SimpleList<T>
     }
 }
 */
+
+var numbers = new List<int> { 10, 12, -100, 55, 17, 22 };
+var filteringStrategySelector = new FilteringStrategySelector();
+
+Console.WriteLine("Select filter: ");
+Console.WriteLine(
+    string.Join(Environment.NewLine, 
+    filteringStrategySelector.FilteringStrategiesName));
+
+var userInput = Console.ReadLine();
+
+var filteringStrategy = new FilteringStrategySelector().Select(userInput);
+List<int> result = new Filter().FilterBy(filteringStrategy, numbers);
+
+Print(result);
+
+Console.ReadKey();
+
+
+void Print(IEnumerable<int> numbers)
+{
+    Console.WriteLine(string.Join(", ", numbers));
+}
+
+public class FilteringStrategySelector
+{
+    private readonly Dictionary<string, Func<int, bool>> _filteringstrategies =
+        new Dictionary<string, Func<int, bool>>
+        {
+            ["Even"] = numbers => numbers % 2 == 0,
+            ["Odd"] = numbers => numbers % 2 == 1,
+            ["Positive"] = numbers => numbers > 0,
+            ["Negative"] = numbers => numbers< 0,
+        };
+
+    public IEnumerable<string> FilteringStrategiesName => _filteringstrategies.Keys;
+
+    public Func<int, bool> Select(string filteringType)
+    {
+        if (!_filteringstrategies.ContainsKey(filteringType))
+        {
+            throw new NotSupportedException(
+                $"{filteringType} is not a valid filter.");
+        }
+        return _filteringstrategies[filteringType];
+    }
+}
+
+public class Filter
+{
+    public List<T> FilterBy<T>(Func<T, bool> predicate, List<T> numbers)
+    {
+        var result = new List<T>();
+        foreach (var number in numbers)
+        {
+            if (predicate(number))
+            {
+                result.Add(number);
+            }
+        }
+        return result;
+    }
+}
--- LambdaExpression/Program.cs
+++ LambdaExpression/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Exercise
+{
+    public Func<string, int> GetLength = text => text.Length;
+
+
+
+    public Func<int> GetRandomNumberBetween1And10 = () => new Random().Next(1,11);
+}
