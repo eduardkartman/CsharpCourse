836cee00de39fb3a9889bfd58e57d872d4e6bd2a
Eduard Kartman <eduard.kartman@yahoo.com>
2023-11-07 14:10:18 +0200

    Updated CSV Project

---
+++ CSV_Project/NewSolution/FastTableData.cs
@@ -0,0 +1,22 @@
+﻿using CsvDataAccess.Interface;
+
+namespace CSV_Project.NewSolution
+{
+    public class FastTableData : ITableData
+    {
+        private readonly List<FastRow> _rows;
+        public int RowCount => _rows.Count;
+        public IEnumerable<string> Columns { get; }
+
+        public FastTableData(IEnumerable<string> columns, List<FastRow> rows)
+        {
+            _rows = rows;
+            Columns = columns;
+        }
+
+        public object GetValue(string columnName, int rowIndex)
+        {
+            return _rows[rowIndex].GetAtColumn(columnName);
+        }
+    }
+}
--- CSV_Project/NewSolution/FastTableDataBuilder.cs
+++ CSV_Project/NewSolution/FastTableDataBuilder.cs
@@ -12,103 +12,39 @@ namespace CSV_Project.NewSolution
 
             foreach (var row in csvData.Rows)
             {
-                var newRowData = new Dictionary<string, object>();
-
+                var newRow = new FastRow();
                 for (int columnIndex = 0; columnIndex < csvData.Columns.Length; ++columnIndex)
                 {
                     var column = csvData.Columns[columnIndex];
                     string valueAsString = row[columnIndex];
-                    object value = ConvertValueToTargetType(valueAsString);
-                    if (value is not null)
+                    if (string.IsNullOrEmpty(valueAsString))
                     {
-                        newRowData[column] = value;
+                        continue;
                     }
-
+                    else if (valueAsString == "TRUE")
+                    {
+                        newRow.AssignCell(column, true);
+                    }
+                    else if (valueAsString == "FALSE")
+                    {
+                        newRow.AssignCell(column, false);
+                    }
+                    else if (valueAsString.Contains(".") && decimal.TryParse(valueAsString, out var valueAsDecimal))
+                    {
+                        newRow.AssignCell(column, valueAsDecimal);
+                    }
+                    else if (int.TryParse(valueAsString, out var valueAsInt))
+                    {
+                        newRow.AssignCell(column, valueAsInt);
+                    }
+                    else
+                    {
+                        newRow.AssignCell(column, valueAsString);
+                    }
+                    
                 }
-
-                resultRows.Add(new FastRow(newRowData));
             }
-
-            return new FastTableData(csvData.Columns, resultRows);
-        }
-
-        private object ConvertValueToTargetType(string value)
-        {
-            if (string.IsNullOrEmpty(value))
-            {
-                return null;
-            }
-            if (value == "TRUE")
-            {
-                return true;
-            }
-            if (value == "FALSE")
-            {
-                return false;
-            }
-            if (value.Contains(".") && decimal.TryParse(value, out var valueAsDecimal))
-            {
-                return valueAsDecimal;
-            }
-            if (int.TryParse(value, out var valueAsInt))
-            {
-                return valueAsInt;
-            }
-            return value;
-        }
-    }
-    public class FastRow
-    {
-        private Dictionary<string, int> _intsData;
-        private Dictionary<string, string> _stringsData;
-        private Dictionary<string, bool> _boolsData;
-        private Dictionary<string, decimal> _decimalsData;
-
-        public void AssignCell(string columnName, bool value)
-        {
-            _boolsData[columnName]= value;
-        }
-        public void AssignCell(string columnName, int value)
-        {
-            _intsData[columnName] = value;
-        }
-        public void AssignCell(string columnName, string value)
-        {
-            _stringsData[columnName] = value;
-        }
-        public void AssignCell(string columnName, decimal value)
-        {
-            _decimalsData[columnName] = value;
-        }
-
-        public object GetAtColumn(string columnName)
-        {
-            if (_data.ContainsKey(columnName))
-            {
-                return _data[columnName];
-            }
-
-            return null;
-        }
-
-
-    }
-
-    public class FastTableData : ITableData
-    {
-        private readonly List<FastRow> _rows;
-        public int RowCount => _rows.Count;
-        public IEnumerable<string> Columns { get; }
-
-        public FastTableData(IEnumerable<string> columns, List<FastRow> rows)
-        {
-            _rows = rows;
-            Columns = columns;
-        }
-
-        public object GetValue(string columnName, int rowIndex)
-        {
-            return _rows[rowIndex].GetAtColumn(columnName);
+            return new FastTableData(csvData.Columns,resultRows);
         }
     }
 }
